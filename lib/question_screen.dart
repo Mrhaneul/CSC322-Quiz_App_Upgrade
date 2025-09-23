0a5f7168e3cf53337e17b157b0a0983f3dd50367
Kipung <nankipung@gmail.com>
2025-09-23 10:03:15 -0700

    frst colab

--- lib/question_screen.dart
+++ lib/question_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:quiz_app/answer_button.dart';
 import 'package:quiz_app/data/questions.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:step_progress_indicator/step_progress_indicator.dart';
+// import 'package:step_progress_indicator/step_progress_indicator.dart';
 import 'package:quiz_app/questions_theme/question_theme.dart';
 
 class QuestionsScreen extends StatefulWidget {
